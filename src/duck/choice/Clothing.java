7b832b1516a68d8d828ae4e88e5657ad9e0fb58b
zakariabanid101 <banidzakaria2@gmail.com>
2025-03-07 12:30:26 +0000

    practice access modifiers

--- src/duck/choice/Clothing.java
+++ src/duck/choice/Clothing.java
@@ -1,7 +1,39 @@
 package duck.choice;
 
 public class Clothing {
-    String description;
-    double price;
-    String size = "M";
+
+    private String description;
+    private double price;
+    private String size;
+    private final int minimumPrice = 10;
+    private final double tax = 0.2;
+
+
+    public double getPrice() {
+        return price + (price * tax);
+    }
+
+    public void setPrice(double price) {
+        if (price < minimumPrice) {
+            this.price = price;
+        } else this.price = minimumPrice;
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
 }
--- src/duck/choice/Main.java
+++ src/duck/choice/Main.java
@@ -6,41 +6,42 @@ import java.lang.reflect.Array;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        double tax = 0.2;
-        double total = 0.0;
-        int measurement = 6;
-
-        Customer c1 = new Customer();
-        c1.name ="pink";
-        System.out.println(c1.name);
-        String string = "HELLO WORLD";
-        System.out.println(string.indexOf("O"));
-        Customer c2 = new Customer();
-        c2.name ="pinky";
-        c2.size = "S";
+//        double tax = 0.2;
+//        double total = 0.0;
+//        int measurement = 6;
 
         Clothing item1 = new Clothing();
         Clothing item2 = new Clothing();
         Clothing item3 = new Clothing();
         Clothing item4 = new Clothing();
 
-        item1.description = "Blue Jacket";
-        item1.price = 20.9;
-        item1.size = "M";
-        item2.description = "Orange T-shirt";
-        item2.price = 10.5;
-        item2.size = "S";
-        item3.description = "Green Scarf";
-        item3.price = 5.0;
-        item3.size = "S";
-        item4.description = "Blue T-shirt";
-        item4.price = 10.5;
-        item4.size = "S";
+        item1.setDescription("Blue Jacket");
+        item1.setPrice(20.9);
+        item1.setSize("M");
+        item2.setDescription("Orange T-shirt");
+        item2.setPrice(10.5);
+        item2.setSize("S");
+        item3.setDescription("Green Scarf");
+        item3.setPrice(5.0);
+        item3.setSize("S");
+        item4.setDescription("Blue T-shirt");
+        item4.setPrice(10.5);
+        item4.setSize("S");
+
+        Customer c1 = new Customer();
+        c1.setName("pink");
+        System.out.println(c1.getName());
+        String string = "HELLO WORLD";
+        System.out.println(string.indexOf("O"));
+        Customer c2 = new Customer();
+        c2.setName("pinky");
+        c2.setSize(3);
+
+        System.out.println("the size of the customer is: " + c2.getSize());
+
 
         //practice switch statement:
-        Clothing[] items = {item1, item2, item3, item4};
-
-
+//        Clothing[] items = {item1, item2, item3, item4};
 
 //        System.out.println("the price of " + item1.description + "is " + item1.price + "and the size is " + item1.size);
 //        System.out.println("the price of " + item2.description + "is " + item2.price + "and the size is " + item2.size);
@@ -67,20 +68,20 @@ public class Main {
 //        }
 //
 //        System.out.println("the size of the customer " + c2.name + "is " + c2.size);
-
-
-        //practicing loops
-        //using if statement to control flow
-        for (Clothing item : items) {
-            System.out.println("the price of " + item.description + "is " + item.price + " and the size is " + item.size);
-            if(c2.size.equals(item.size)) {
-                total = total + item.price;
-                if ( total>15){
-                    break;
-                }
-            }
-        }
-        System.out.println(total);
+//
+//
+//        //practicing loops
+//        //using if statement to control flow
+//        for (Clothing item : items) {
+//            System.out.println("the price of " + item.description + "is " + item.price + " and the size is " + item.size);
+//            if(c2.size.equals(item.size)) {
+//                total = total + item.price;
+//                if ( total>15){
+//                    break;
+//                }
+//            }
+//        }
+//        System.out.println(total);
 
     }
 }
